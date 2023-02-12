64b3774426731dc182e6156a73a6987def9090c4
AlexandrVikhristyk <alexvikhristyk@gmail.com>
2023-02-12 19:40:08 +0200

    Initial commit

---
+++ src/main/java/com/gachigang/ontherun/OntherunApplication.java
@@ -0,0 +1,13 @@
+package com.gachigang.ontherun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntherunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntherunApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gachigang/ontherun/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gachigang.ontherun;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OntherunApplication.class);
+    }
+
+}
