9392409685089d255490447cba5eba772ded60b8
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-02-13 13:08:49 +0200

    #8 - Add User Entity

---
+++ src/main/java/com/gachigang/ontherun/common/validator/EmailValidator.java
@@ -0,0 +1,25 @@
+package com.gachigang.ontherun.common.validator;
+
+import com.gachigang.ontherun.common.ApplicationConstants;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.NonNull;
+
+/**
+ * Class validate an email address using a regular expression.
+ * The class implements the {@link ConstraintValidator} interface.
+ */
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String>{
+
+    @Override
+    public boolean isValid(@NonNull String email,
+                           @NonNull ConstraintValidatorContext context) {
+        return validateEmail(email);
+    }
+
+    private boolean validateEmail(@NonNull final String email) {
+        final var matcher = ApplicationConstants.Validation.EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
+    }
+}
---
+++ src/main/java/com/gachigang/ontherun/persistence/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.gachigang.ontherun.persistence.repository;
+
+import com.gachigang.ontherun.persistence.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<Long, User> {
+}
