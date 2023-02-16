fa4478de57e9a9a664afe07f5801db4738531743
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-02-16 21:09:51 +0200

    #24 - Add tests for UserService - Add test for 2 methods save and getByEmail - Change the variable in EmailValidator

---
+++ src/test/java/com/gachigang/ontherun/service/UserServiceTest.java
@@ -0,0 +1,64 @@
+package com.gachigang.ontherun.service;
+
+import com.gachigang.ontherun.persistence.entity.User;
+import com.gachigang.ontherun.persistence.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.BDDAssumptions.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    private final static User USER = User.builder()
+            .id(1L)
+            .surname("Lena")
+            .lastname("Golovach")
+            .login("testLogin")
+            .email("test@email.com")
+            .dateOfBirth("31/12/2002")
+            .country("Ukraine")
+            .city("Kyiv")
+            .password("yashalava")
+            .build();
+
+    private final static String EMAIL = "test@email.com";
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testSaveUser_successfully() {
+        when(userRepository.save(USER))
+                .thenReturn(USER);
+
+        final User createdUser = userService.save(USER);
+
+        verify(userRepository, times(1)).save(USER);
+        assertNotNull(createdUser);
+    }
+
+
+    @Test
+    public void testGetUserByEmail_isCorrect() {
+        when(userRepository.findUserByEmail(EMAIL))
+                .thenReturn(Optional.ofNullable(USER));
+
+        final User actualUser = userService.getByEmail(EMAIL);
+
+        verify(userRepository, times(1)).findUserByEmail(EMAIL);
+        assertEquals(USER, actualUser);
+    }
+}
