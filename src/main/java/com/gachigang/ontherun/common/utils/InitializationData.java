7117b800d2d219deba1694864d2dde9bac1dc23f
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-02-16 23:52:02 +0200

    #28 - Implement CommandLineRunner - Add new files InitializationData and RoleRepository

--- src/main/java/com/gachigang/ontherun/common/utils/InitializationData.java
+++ src/main/java/com/gachigang/ontherun/common/utils/InitializationData.java
@@ -1,5 +1,6 @@
-package com.gachigang.ontherun.utils;
+package com.gachigang.ontherun.common.utils;
 
+import com.gachigang.ontherun.common.enums.UserRole;
 import com.gachigang.ontherun.persistence.entity.Role;
 import com.gachigang.ontherun.persistence.repository.RoleRepository;
 import lombok.AllArgsConstructor;
@@ -14,16 +15,16 @@ public class InitializationData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Role role1 = new Role();
-        Role role2 = new Role();
-        Role role3= new Role();
+        Role staff = new Role();
+        staff.setName(UserRole.STAFF.getRole());
+        roleRepository.save(staff);
 
-        role1.setName("STAFF");
-        role2.setName("MANAGER");
-        role3.setName("USER");
+        Role manager = new Role();
+        manager.setName(UserRole.MANAGER.getRole());
+        roleRepository.save(manager);
 
-        roleRepository.save(role1);
-        roleRepository.save(role2);
-        roleRepository.save(role3);
+        Role user = new Role();
+        user.setName(UserRole.USER.getRole());
+        roleRepository.save(user);
     }
 }
