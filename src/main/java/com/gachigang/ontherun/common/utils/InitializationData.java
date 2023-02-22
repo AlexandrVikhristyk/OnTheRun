3412f7875582c9e8fe42a2c3d70a4ea20b13c026
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-02-23 00:46:53 +0200

    #32 - Redone the InitializationData class Modified InitializationData class to use the @Value annotation to inject the value of spring.jpa.hibernate.ddl-auto property into ddlAuto field. Updated run method to conditionally execute the initialization logic based on the value of ddlAuto. The class will only execute if ddl-auto is set to a value other than "create" or "create-drop".

--- src/main/java/com/gachigang/ontherun/common/utils/InitializationData.java
+++ src/main/java/com/gachigang/ontherun/common/utils/InitializationData.java
@@ -4,6 +4,7 @@ import com.gachigang.ontherun.common.enums.UserRole;
 import com.gachigang.ontherun.persistence.entity.Role;
 import com.gachigang.ontherun.persistence.repository.RoleRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,17 @@ import org.springframework.stereotype.Component;
 public class InitializationData implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    @Value("${spring.jpa.properties.hibernate.ddl-auto}")
+    private static String ddlAuto;
 
     @Override
     public void run(String... args) throws Exception {
-        for(UserRole userRole : UserRole.values()) {
-            Role role = new Role();
-            role.setName(userRole.getRole());
-            roleRepository.save(role);
+        if (!ddlAuto.equals("create") && !ddlAuto.equals("create-drop")) {
+            for (UserRole userRole : UserRole.values()) {
+                Role role = new Role();
+                role.setName(userRole.getRole());
+                roleRepository.save(role);
+            }
         }
     }
 }
