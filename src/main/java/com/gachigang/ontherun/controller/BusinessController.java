57f07a843795e7ef663b59162911beebf804d424
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-02-28 20:21:21 +0200

    #35 - Fix the class BusinessController

--- src/main/java/com/gachigang/ontherun/controller/BusinessController.java
+++ src/main/java/com/gachigang/ontherun/controller/BusinessController.java
@@ -3,6 +3,7 @@ package com.gachigang.ontherun.controller;
 import com.gachigang.ontherun.persistence.entity.Business;
 import com.gachigang.ontherun.service.BusinessService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/business")
+@Slf4j
 public class BusinessController {
 
     private final BusinessService businessService;
@@ -33,6 +35,7 @@ public class BusinessController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBusinessById(@PathVariable final Long id) {
         businessService.deleteBusinessById(id);
+        log.info("Business with id: {} was deleted", id);
         return ResponseEntity.ok().build();
     }
 }
