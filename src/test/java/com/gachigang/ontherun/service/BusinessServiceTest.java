f0d13529c7e95cead0de423d9e21ed1163861f07
d4ng3r228 <dangersof228@gmail.com>
2023-02-28 20:47:20 +0200

    #19 Add GET /business by userId endpoint

--- src/test/java/com/gachigang/ontherun/service/BusinessServiceTest.java
+++ src/test/java/com/gachigang/ontherun/service/BusinessServiceTest.java
@@ -1,5 +1,7 @@
 package com.gachigang.ontherun.service;
 
+import com.gachigang.ontherun.persistence.entity.Business;
+import com.gachigang.ontherun.persistence.entity.User;
 import com.gachigang.ontherun.persistence.repository.BusinessRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,10 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BusinessServiceTest {
+
     @Mock
     private BusinessRepository businessRepository;
 
@@ -21,5 +28,21 @@ class BusinessServiceTest {
     void testGetAllBusiness() {
         businessService.getAllBusiness();
         verify(businessRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testFindBusinessByOwners() {
+        Business business = Business.builder().
+                id(1L)
+                .name("testName").
+                country("Ukraine").
+                city("Kyiv").
+                build();
+        User user = User.builder().
+                businesses(Collections.singleton(business)).
+                build();
+        when(businessRepository.findBusinessByOwners(user)).thenReturn(Collections.singletonList(business));
+        List<Business> result = businessService.findBusinessByOwners(user);
+        assertNotNull(result);
     }
 }
