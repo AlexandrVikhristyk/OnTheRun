05726e405dd3a076d9b4bcbb064beb57358585da
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-03-08 11:19:31 +0200

    #37 - Implemented MapStruct

---
+++ src/main/java/com/gachigang/ontherun/common/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.gachigang.ontherun.common.mapper;
+
+import com.gachigang.ontherun.model.dto.UserDto;
+import com.gachigang.ontherun.persistence.entity.User;
+import org.mapstruct.Mapper;
+
+/**
+ * This interface defines methods for mapping between the {@link User} and {@link UserDto} classes.
+ */
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    /**
+     * Maps a {@link User} object to a {@link UserDto} object.
+     *
+     * @param user The {@link User} object to be mapped.
+     * @return The resulting {@link UserDto} object.
+     */
+    UserDto userToUserDto(User user);
+
+    /**
+     * Maps a {@link UserDto} object to a {@link User} object.
+     *
+     * @param userDto The {@link UserDto} object to be mapped.
+     * @return The resulting {@link User} object.
+     */
+    User userDtoToUser(UserDto userDto);
+}
