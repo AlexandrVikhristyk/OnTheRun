00009b46dee4206ac565505a8226baaeceaaccdf
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-07-30 20:05:04 +0300

    #41 - I`ve rewritten the class BaseSecurityConfig from 5.8 ver to 6.1 ver.

--- src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
+++ src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
@@ -3,23 +3,35 @@ package com.gachigang.ontherun.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Security configuration class.
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class BaseSecurityConfig {
 
+    private final AuthenticationProvider authenticationProvider;
+    /**
+     * Configuration of security.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/login", "/auth/register").permitAll()
-                .anyRequest().authenticated();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider);
 
         return http.build();
     }
