1a4af897a74b1e9cbd720af6f9c999a025c0151e
d4ng3r228 <dangersof228@gmail.com>
2023-09-19 14:41:33 +0300

    Added logout functional

--- src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
+++ src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
@@ -1,13 +1,16 @@
 package com.gachigang.ontherun.config.security;
 
+import com.gachigang.ontherun.service.CustomLogoutHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 /**
  * Security configuration class.
@@ -19,6 +22,7 @@ public class BaseSecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationEntryPointImpl authenticationEntryPoint;
+    private final CustomLogoutHandler logoutHandler;
 
     /**
      * Configuration of security.
@@ -33,6 +37,11 @@ public class BaseSecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/businesses/*").permitAll()
                         .anyRequest().authenticated())
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+                        .logoutUrl("/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                        .permitAll())
                 .authenticationProvider(authenticationProvider)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint));
 
