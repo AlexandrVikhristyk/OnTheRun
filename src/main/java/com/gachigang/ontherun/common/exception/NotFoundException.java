a69860b1d25d955f091f3b695f595e309f65cbe0
d4ng3r228 <dangersof228@gmail.com>
2023-09-19 20:55:05 +0300

    Fix

--- src/main/java/com/gachigang/ontherun/common/exception/NotFoundException.java
+++ src/main/java/com/gachigang/ontherun/common/exception/NotFoundException.java
@@ -1,11 +1,16 @@
 package com.gachigang.ontherun.common.exception;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * Exception class representing HTTP 404(Not found).
  */
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException(String message) {
-        super(message);
+    @Value("${not.found.error.message}")
+    private static String notFoundErrorMessage;
+
+    public NotFoundException() {
+        super(notFoundErrorMessage);
     }
 }
