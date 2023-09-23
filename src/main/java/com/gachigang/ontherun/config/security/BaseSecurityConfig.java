fc251ddf1336e52226b6068a175c8d233a69b595
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-09-23 21:04:45 +0300

    #45 - Fixed some files

--- src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
+++ src/main/java/com/gachigang/ontherun/config/security/BaseSecurityConfig.java
@@ -28,7 +28,6 @@ public class BaseSecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
