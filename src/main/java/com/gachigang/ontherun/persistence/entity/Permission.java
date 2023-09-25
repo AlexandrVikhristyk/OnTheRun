6d7a0801185faf65922e60c2d2ede2f43eff1adf
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-09-25 13:19:45 +0300

    #45 - Fixed some files

--- src/main/java/com/gachigang/ontherun/persistence/entity/Permission.java
+++ src/main/java/com/gachigang/ontherun/persistence/entity/Permission.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-import org.hibernate.envers.Audited;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
