e5e1c49871fda7914afef206e8656b26e96ad91e
harpestes <dikaloandrij@gmail.com>
2023-09-27 17:03:31 +0300

    #21 - Fix DepartmentService and DepartmentRepository

--- src/main/java/com/gachigang/ontherun/persistence/repository/DepartmentRepository.java
+++ src/main/java/com/gachigang/ontherun/persistence/repository/DepartmentRepository.java
@@ -5,7 +5,13 @@ import com.gachigang.ontherun.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Basic repository for the department.
+ * The {@code DepartmentRepository} interface defines methods for interacting with
+ * {@link Department} entities.
+ *<p>
+ * This repository allows querying and managing departments in the system based on
+ * various criteria.
+ *
+ * @see Department
  */
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department findDepartmentByUsersContains(User user);
--- src/main/java/com/gachigang/ontherun/service/DepartmentService.java
+++ src/main/java/com/gachigang/ontherun/service/DepartmentService.java
@@ -1,25 +1,25 @@
 package com.gachigang.ontherun.service;
 
+import com.gachigang.ontherun.common.exception.NotFoundException;
 import com.gachigang.ontherun.persistence.entity.Department;
 import com.gachigang.ontherun.persistence.repository.DepartmentRepository;
 import com.gachigang.ontherun.persistence.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
-@AllArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class DepartmentService {
+
     private final DepartmentRepository departmentRepository;
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public Department getDepartmentByUserId(@NonNull final Long id) {
-        Department foundDepartment = departmentRepository.findDepartmentByUsersContains(userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("User with id " + id + " not found")));
-        if(foundDepartment == null) {
-            throw new RuntimeException("There is no department for this user");
-        }
-        else return foundDepartment;
+        return departmentRepository.findDepartmentByUsersContains(userRepository.findById(id)
+                .orElseThrow(NotFoundException::new));
     }
 }
