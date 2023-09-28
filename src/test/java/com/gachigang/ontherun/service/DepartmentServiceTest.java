e4a0ab1f5ef65cb4351410b05b2bd6f20ad98a30
harpestes <dikaloandrij@gmail.com>
2023-09-28 15:57:23 +0300

    #21 - Fix DepartmentServiceTest

--- src/test/java/com/gachigang/ontherun/service/DepartmentServiceTest.java
+++ src/test/java/com/gachigang/ontherun/service/DepartmentServiceTest.java
@@ -1,5 +1,6 @@
 package com.gachigang.ontherun.service;
 
+import com.gachigang.ontherun.common.exception.NotFoundException;
 import com.gachigang.ontherun.persistence.entity.Department;
 import com.gachigang.ontherun.persistence.entity.User;
 import com.gachigang.ontherun.persistence.repository.DepartmentRepository;
@@ -50,20 +51,12 @@ public class DepartmentServiceTest {
         Long userId = 2L;
         when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
-        assertThrows(IllegalArgumentException.class, () -> departmentService.getDepartmentByUserId(userId));
+        assertThrows(NotFoundException.class, () -> departmentService.getDepartmentByUserId(userId));
     }
 
     @Test
     public void testGetDepartmentByUserIdUserWithNoDepartment() {
-        Long userId = 3L;
-        User user = User.builder()
-                .id(userId)
-                .lastname("Doe")
-                .build();
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(departmentRepository.findDepartmentByUsersContains(user)).thenReturn(null);
-
-        assertThrows(RuntimeException.class, () -> departmentService.getDepartmentByUserId(userId));
+        assertThrows(RuntimeException.class, () -> departmentService.getDepartmentByUserId(4L));
     }
 }
 
