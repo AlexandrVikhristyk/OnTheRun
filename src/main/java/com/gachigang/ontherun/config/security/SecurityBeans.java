4ea1914d7eb7db8366834d5bf8b874f0dbd07e9f
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-10-11 00:28:31 +0300

    #54 - Configurated CORS

--- src/main/java/com/gachigang/ontherun/config/security/SecurityBeans.java
+++ src/main/java/com/gachigang/ontherun/config/security/SecurityBeans.java
@@ -4,6 +4,7 @@ import com.gachigang.ontherun.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,26 +17,25 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
-import static com.gachigang.ontherun.common.ApplicationConstants.Security.ALLOWEDMETHODS;
-import static com.gachigang.ontherun.common.ApplicationConstants.Security.ALLOWEDORIGINS;
+import java.util.List;
+import java.util.Objects;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityBeans {
 
     private final UserRepository userRepository;
+    private final Environment environment;
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        return username ->userRepository.findUserByEmail(username)
-                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider =new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(bCryptPasswordEncoder());
 
@@ -43,7 +43,7 @@ public class SecurityBeans {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
@@ -55,9 +55,10 @@ public class SecurityBeans {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(ALLOWEDORIGINS));
-        configuration.setAllowedMethods(Arrays.asList(ALLOWEDMETHODS));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        configuration.setAllowedOrigins(List.of(Objects.requireNonNull(environment.getProperty("application.front.url"))));
+        configuration.setAllowedMethods(List.of("POST", "GET"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
