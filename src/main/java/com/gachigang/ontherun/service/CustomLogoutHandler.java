6116029d8d2c8c5b21d8e9f2a0a52056096c3997
Andrew Konoplastiy <konoplastiy@gmail.cim>
2023-10-22 19:42:15 +0300

    #58 - CustomLogoutHandler revokes tokens

--- src/main/java/com/gachigang/ontherun/service/CustomLogoutHandler.java
+++ src/main/java/com/gachigang/ontherun/service/CustomLogoutHandler.java
@@ -1,18 +1,41 @@
 package com.gachigang.ontherun.service;
 
+import com.gachigang.ontherun.persistence.entity.Token;
+import com.gachigang.ontherun.persistence.repository.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CustomLogoutHandler implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
     @Override
-    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        SecurityContextHolder.clearContext();
+    public void logout(
+            @NonNull final HttpServletRequest request,
+            @NonNull final HttpServletResponse response,
+            @NonNull final Authentication authentication
+    ) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        Token storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
     }
 }
